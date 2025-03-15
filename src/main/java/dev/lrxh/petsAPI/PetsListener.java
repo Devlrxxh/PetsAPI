43aff2aa51d5035b077c28137778e6342f0d1c89
Devlrxxh <seabearvalorant@gmail.com>
2025-03-15 18:06:25 +0100

    Fixed visibility issues

--- src/main/java/dev/lrxh/petsAPI/PetsListener.java
+++ src/main/java/dev/lrxh/petsAPI/PetsListener.java
@@ -1,33 +1,22 @@
 package dev.lrxh.petsAPI;
 
-import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerHideEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.event.player.PlayerShowEntityEvent;
 
 public class PetsListener implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-        PetsAPI.load(event.getPlayer());
+        Bukkit.getScheduler().runTaskLater(PetsAPI.instance, () -> {
+            PetsAPI.addIgnoredPlayer(event.getPlayer());
+            PetsAPI.removeIgnoredPlayer(event.getPlayer());
+        }, 10L);
     }
 
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
         PetsAPI.kill(event.getPlayer());
-    }
-
-    @EventHandler
-    public void onShow(PlayerShowEntityEvent event) {
-        if (event.getEntity() instanceof Player player)
-            PetsAPI.load(player, event.getPlayer());
-    }
-
-    @EventHandler
-    public void onHide(PlayerHideEntityEvent event) {
-        if (event.getEntity() instanceof Player player)
-            PetsAPI.hide(player, event.getPlayer());
     }
 }
